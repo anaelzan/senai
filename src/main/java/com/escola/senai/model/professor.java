0a03971083be2a566a025c4de954f7bb5f6ef601
anaelzan. <anaelzanunespedro@gmail.com>
2025-04-12 11:22:06 -0300

    feat

--- src/main/java/com/escola/senai/model/professor.java
+++ src/main/java/com/escola/senai/model/professor.java
@@ -1,4 +1,39 @@
 package com.escola.senai.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class professor {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    public void setDisciplinaProfessor(String disciplinaProfessor) {
+        this.disciplinaProfessor = disciplinaProfessor;
+    }
 }
